b6f6872815672ca6d5fca1691c28683aabc871a7
robertocolman <robertocolman93@gmail.com>
2021-12-09 20:25:47 -0300

    usuario y registro

--- ods/odsApp/urls.py
+++ ods/odsApp/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from odsApp import views
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import registro
+
 
 urlpatterns = [
 	path('', views.home, name="home"),
-	path('objetivo1', views.objetivo1, name="objetivo1")
+	path('objetivo1', views.objetivo1, name="objetivo1"),
+	path('registro', registro, name="registro"),
+
 	
 ]
--- ods/odsApp/views.py
+++ ods/odsApp/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForms
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def home(request):
 	return render(request, "odsApp/index.html")
 
 def objetivo1(request):
 	return render(request, "odsApp/objetivo1.html")
+
+def registro(request):
+	data ={
+		'form': CustomUserCreationForms()
+	}
+
+	if request.method == 'POST':
+		formulario = CustomUserCreationForms(data=request.POST)
+		if formulario.is_valid():
+			formulario.save()
+			user = authenticate(username=formulario.cleaned_data["username"], password=formulario.cleaned_data["password1"])
+			login(request, user)
+			messages.success(request, "te has registrado correctamente")
+			return redirect(to="home")
+		data["form"] = formulario
+	return render(request, "registration/registro.html", data)
