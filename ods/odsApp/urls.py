dc5d72db8afd790019673317fbb34a782fb03d97
robertocolman <robertocolman93@gmail.com>
2021-12-09 21:55:54 -0300

    objetivos

--- ods/odsApp/urls.py
+++ ods/odsApp/urls.py
@@ -8,7 +8,25 @@ from .views import registro
 urlpatterns = [
 	path('', views.home, name="home"),
 	path('objetivo1', views.objetivo1, name="objetivo1"),
+	path('objetivo2', views.objetivo2, name="objetivo2"),
+	path('objetivo3', views.objetivo3, name="objetivo3"),
+	path('objetivo4', views.objetivo4, name="objetivo4"),
+	path('objetivo5', views.objetivo5, name="objetivo5"),
+	path('objetivo6', views.objetivo6, name="objetivo6"),
+	path('objetivo7', views.objetivo7, name="objetivo7"),
+	path('objetivo8', views.objetivo8, name="objetivo8"),
+	path('objetivo9', views.objetivo9, name="objetivo9"),
+	path('objetivo10', views.objetivo10, name="objetivo10"),
+	path('objetivo11', views.objetivo11, name="objetivo11"),
+	path('objetivo12', views.objetivo12, name="objetivo12"),
+	path('objetivo13', views.objetivo13, name="objetivo13"),
+	path('objetivo14', views.objetivo14, name="objetivo14"),
+	path('objetivo15', views.objetivo15, name="objetivo15"),
+	path('objetivo16', views.objetivo16, name="objetivo16"),
+	path('objetivo17', views.objetivo17, name="objetivo17"),
+	path('leermas', views.leermas, name="leermas"),
 	path('registro', registro, name="registro"),
+
 
 	
 ]
--- ods/odsApp/views.py
+++ ods/odsApp/views.py
@@ -7,7 +7,58 @@ def home(request):
 	return render(request, "odsApp/index.html")
 
 def objetivo1(request):
-	return render(request, "odsApp/objetivo1.html")
+	return render(request, "odsApp/objetivos/objetivo1.html")
+
+def objetivo2(request):
+	return render(request, "odsApp/objetivos/objetivo2.html")
+
+def objetivo3(request):
+	return render(request, "odsApp/objetivos/objetivo3.html")
+
+def objetivo4(request):
+	return render(request, "odsApp/objetivos/objetivo4.html")
+
+def objetivo5(request):
+	return render(request, "odsApp/objetivos/objetivo5.html")
+
+def objetivo6(request):
+	return render(request, "odsApp/objetivos/objetivo6.html")
+
+def objetivo7(request):
+	return render(request, "odsApp/objetivos/objetivo7.html")
+
+def objetivo8(request):
+	return render(request, "odsApp/objetivos/objetivo8.html")
+
+def objetivo9(request):
+	return render(request, "odsApp/objetivos/objetivo9.html")
+
+def objetivo10(request):
+	return render(request, "odsApp/objetivos/objetivo10.html")
+
+def objetivo11(request):
+	return render(request, "odsApp/objetivos/objetivo11.html")
+
+def objetivo12(request):
+	return render(request, "odsApp/objetivos/objetivo12.html")
+
+def objetivo13(request):
+	return render(request, "odsApp/objetivos/objetivo13.html")
+
+def objetivo14(request):
+	return render(request, "odsApp/objetivos/objetivo14.html")
+
+def objetivo15(request):
+	return render(request, "odsApp/objetivos/objetivo15.html")
+
+def objetivo16(request):
+	return render(request, "odsApp/objetivos/objetivo16.html")
+
+def objetivo17(request):
+	return render(request, "odsApp/objetivos/objetivo17.html")
+
+def leermas(request):
+	return render(request, "odsApp/objetivos/leermas.html")
 
 def registro(request):
 	data ={
